71abb53f00944d5925beee690ce897ba29112c46
Nora Pete <norapete19@gmail.com>
2019-10-07 13:58:45 +0200

    feat(Jenkinsfile):add deploy to aws script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+aws s3 cp ./Dockerrun.aws.json \
+  s3://elasticbeanstalk-eu-west-2-124429370407/helloFromDocker/Dockerrun.aws.json
+
+aws elasticbeanstalk create-application-version \
+  --application-name "helloFromDocker" \
+  --version-label helloFromDocker \
+  --source-bundle S3Bucket="elasticbeanstalk-eu-west-2-124429370407",S3Key="helloFromDocker/Dockerrun.aws.json" \
+  --auto-create-application
+
+aws elasticbeanstalk update-environment \
+  --application-name "helloFromDocker" \
+  --environment-name "helloFromDocker-dev" \
+--version-label helloFromDocker
