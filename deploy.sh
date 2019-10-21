abda270a0b0744b394cf88f2dc4ddc46b9712d9c
Nora Pete <norapete19@gmail.com>
2019-10-21 16:36:12 +0200

    feat(deploy):add versioning at step deploy to AWS

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 aws s3 cp ./Dockerrun.aws.json \
-  s3://elasticbeanstalk-eu-west-2-124429370407/helloFromDocker/Dockerrun.aws.json
+  s3://elasticbeanstalk-eu-west-2-124429370407/helloFromDocker/$BUILD_ID/Dockerrun.aws.json
 
 aws elasticbeanstalk create-application-version \
   --application-name "helloFromDocker" \
-  --version-label helloFromDocker \
-  --source-bundle S3Bucket="elasticbeanstalk-eu-west-2-124429370407",S3Key="helloFromDocker/Dockerrun.aws.json" \
+  --version-label helloFromDocker-$BUILD_ID \
+  --source-bundle S3Bucket="elasticbeanstalk-eu-west-2-124429370407",S3Key="helloFromDocker/$BUILD_ID/Dockerrun.aws.json" \
   --auto-create-application
 
 aws elasticbeanstalk update-environment \
   --application-name "helloFromDocker" \
   --environment-name "helloFromDocker-prod" \
---version-label helloFromDocker
+--version-label helloFromDocker-$BUILD_ID
